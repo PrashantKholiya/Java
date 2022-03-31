03e8cc96b86f223039417c8edab91a0adfbe5fbc
PrashantKholiya <90739241+PrashantKholiya@users.noreply.github.com>
2022-03-31 23:49:54 +0530

    Delete unit1 directory

--- unit1/Unit1_P2_Task2.java
+++
@@ -1,9 +0,0 @@
-public class Unit1_P2_Task2 {
-    public static final double pi=3.14;
-    public static void main(String[] args)
-    {
-        final int a=20;
-        System.out.println(pi);
-        System.out.println(a);
-    }
-}
